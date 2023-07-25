963e6c692a68b0093d7e2ef32958d2d24551f423
Mycroft <mycroft.tovarish@gmail.com>
2023-07-24 19:31:17 -0700

    Update RemoteNode.cpp

--- Meridian59.Ogre.Client/RemoteNode.cpp
+++ Meridian59.Ogre.Client/RemoteNode.cpp
@@ -17,12 +17,12 @@ namespace Meridian59 { namespace Ogre
       // create scenenode
       const ::Ogre::String& ostr_scenenodename = 
          PREFIX_REMOTENODE_SCENENODE + ::Ogre::StringConverter::toString(roomObject->ID);
-      Logger::Log(MODULENAME, LogType::Info, "createremotescenenode" + ::Ogre::StringConverter::toString(roomObject->ID));
+      Logger::Log(MODULENAME, LogType::Info, "createremotescenenode" + (roomObject->ID).to_string());
       try {
       SceneNode = SceneManager->getRootSceneNode()->createChildSceneNode(ostr_scenenodename);
       }
       catch(...) {
-	      Logger::Log(MODULENAME, LogType::Info, "caught exception in RemoteNode on scene " + ::Ogre::StringConverter::toString(roomObject->ID));
+	      Logger::Log(MODULENAME, LogType::Info, "caught exception in RemoteNode on scene " + (roomObject->ID).to_string());
       }
 	      
       SceneNode->setFixedYawAxis(true);
